fcfefe9302c5c75a8c1612e6ff3449a8c26bf755
Joaquin-B-S <joaquinkappa@hotmail.com>
2025-06-05 20:51:18 -0400

    galeria con js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    crearGaleria();
+});
 
+function crearGaleria() {
+    const galeria = document.querySelector(".galeria-imagenes");
+    const cantidadImagenes = 16;
+
+    for (let i = 1; i <= cantidadImagenes; i++) {
+        const imagen = document.createElement("IMG");
+        imagen.src = `src/img/gallery/full/${i}.jpg`;
+        imagen.alt = `Imagen galeria ${i}`;
+
+        galeria.appendChild(imagen);
+    }
+}
