e0ab6627888d81d71d7cf1f5de291d6ba58684bb
Joaquin-B-S <joaquinkappa@hotmail.com>
2025-06-05 21:17:53 -0400

    agregando modal a las imagenes

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,38 @@ function crearGaleria() {
         imagen.src = `src/img/gallery/full/${i}.jpg`;
         imagen.alt = `Imagen galeria ${i}`;
 
+        imagen.onclick = function () {
+            mostrarImagen(i);
+        };
+
         galeria.appendChild(imagen);
     }
+}
+
+function mostrarImagen(indice) {
+    const modal = document.createElement("DIV");
+    modal.classList.add("modal");
+
+    const imagen = document.createElement("IMG");
+    imagen.src = `src/img/gallery/full/${indice}.jpg`;
+    imagen.alt = `Imagen galeria ${indice}`;
+
+    modal.appendChild(imagen);
+
+    modal.onclick = cerrarNodal;
+
+    const body = document.querySelector("body");
+    body.classList.add("overflow-hidden");
+    body.appendChild(modal);
+}
+
+function cerrarNodal() {
+    const modal = document.querySelector(".modal");
+    modal.classList.add("fade-out");
+    setTimeout(() => {
+        modal?.remove();
+
+        const body = document.querySelector("body");
+        body.classList.remove("overflow-hidden");
+    }, 500);
 }
