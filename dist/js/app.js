fbefa7907be84a2c842bbe3cb187839481c03949
Joaquin-B-S <joaquinkappa@hotmail.com>
2025-06-06 23:52:52 -0400

    smooth al scroll

--- dist/js/app.js
+++ dist/js/app.js
@@ -91,3 +91,19 @@ function resaltarEnlace() {
         });
     });
 }
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll(".navegacion-principal a");
+
+    enlaces.forEach((enlace) => {
+        enlace.addEventListener("click", function (e) {
+            e.preventDefault();
+            const seccion = document.querySelector(
+                e.target.attributes.href.value
+            );
+            seccion.scrollIntoView({
+                behavior: "smooth",
+            });
+        });
+    });
+}
