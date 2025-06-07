f0ebb20dc02ac2322a281e59c135034f352ce311
Joaquin-B-S <joaquinkappa@hotmail.com>
2025-06-07 00:14:45 -0400

    performance web

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 import { src, dest, watch, series } from "gulp";
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
+import terser from "gulp-terser";
 
 const sass = gulpSass(dartSass);
 
 export function js(done) {
-    src("src/js/app.js").pipe(dest("dist/js"));
+    src("src/js/app.js").pipe(terser()).pipe(dest("dist/js"));
 
     done();
 }
 
 export function css(done) {
     src("src/scss/app.scss", { sourcemaps: true })
-        .pipe(sass().on("error", sass.logError))
+        .pipe(sass({ style: "compressed" }).on("error", sass.logError))
         .pipe(dest("dist/css", { sourcemaps: true }));
 
     done();
